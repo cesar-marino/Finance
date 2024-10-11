876795f47852493a3e5c2a76096fe81dedc28483
César Marino <cesarmarino00@gmail.com>
2024-10-11 16:07:09 -0300

    add  update tag use case

---
+++ src/Application/Finance.Application/UseCases/Tag/UpdateTag/UpdateTagRequest.cs
@@ -0,0 +1,13 @@
+﻿using Finance.Application.UseCases.Tag.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Tag.UpdateTag
+{
+    public class UpdateTagRequest(
+        Guid tagId,
+        string name) : IRequest<TagResponse>
+    {
+        public Guid TagId { get; private set; } = tagId;
+        public string Name { get; private set; } = name;
+    }
+}
