be3dedfd948f77a5f7e1c54e6656d8afacf80524
César Marino <cesarmarino00@gmail.com>
2024-10-11 15:05:40 -0300

    add disable tag use case

---
+++ src/Application/Finance.Application/UseCases/Tag/DisableTag/DisableTagRequest.cs
@@ -0,0 +1,10 @@
+﻿using Finance.Application.UseCases.Tag.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Tag.DisableTag
+{
+    public class DisableTagRequest(Guid tagId) : IRequest<TagResponse>
+    {
+        public Guid TagId { get; private set; } = tagId;
+    }
+}
---
+++ src/Application/Finance.Application/UseCases/Tag/DisableTag/IDisableTagHandler.cs
@@ -0,0 +1,9 @@
+﻿using Finance.Application.UseCases.Tag.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Tag.DisableTag
+{
+    public interface IDisableTagHandler : IRequestHandler<DisableTagRequest, TagResponse>
+    {
+    }
+}
