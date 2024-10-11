353cf11d0b06ee86b5f735c3fed3a9db1a6d311d
César Marino <cesarmarino00@gmail.com>
2024-10-11 16:18:49 -0300

    ensure rethrow same exception that UpdateAsync throws

--- test/Finance.Test.UnitTests/Application/UseCases/Tag/UpdateTag/UpdateTagHandlerTest.cs
+++ test/Finance.Test.UnitTests/Application/UseCases/Tag/UpdateTag/UpdateTagHandlerTest.cs
@@ -1,4 +1,5 @@
 ﻿using Finance.Application.UseCases.Tag.UpdateTag;
+using Finance.Domain.Entities;
 using Finance.Domain.Exceptions;
 using Finance.Domain.Repositories;
 using FluentAssertions;
@@ -35,6 +36,27 @@ namespace Finance.Test.UnitTests.Application.UseCases.Tag.UpdateTag
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Tag not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatUpdateAsyncThrows))]
+        [Trait("Unit/UseCase", "Tag - UpdateTag")]
+        public async Task ShouldRethrowSameExceptionThatUpdateAsyncThrows()
+        {
+            var tag = _fixture.MakeTagEntity();
+            _tagRepositoryMock
+                .Setup(x => x.FindAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(tag);
+
+            _tagRepositoryMock
+                .Setup(x => x.UpdateAsync(It.IsAny<TagEntity>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakeUpdateTagRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
         }
     }
 }
