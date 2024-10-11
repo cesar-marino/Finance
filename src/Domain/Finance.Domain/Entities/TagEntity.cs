5a231ce3b6cf46608d202f9380710cc6cfa6c842
César Marino <cesarmarino00@gmail.com>
2024-10-11 14:19:06 -0300

    add tag entity

---
+++ src/Domain/Finance.Domain/Entities/TagEntity.cs
@@ -0,0 +1,27 @@
+﻿using Finance.Domain.SeedWork;
+
+namespace Finance.Domain.Entities
+{
+    public class TagEntity : AggregateRoot
+    {
+        public bool Active { get; private set; }
+        public string Name { get; private set; }
+
+        public TagEntity(string name)
+        {
+            Active = true;
+            Name = name;
+        }
+
+        public TagEntity(
+            Guid tagId,
+            bool active,
+            string name,
+            DateTime createdAt,
+            DateTime updatedAt) : base(tagId, createdAt, updatedAt)
+        {
+            Active = active;
+            Name = name;
+        }
+    }
+}
