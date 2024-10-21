be3b7e72b09b89b4dbe528cf1d603925f58ae842
César Marino <cesarmarino00@gmail.com>
2024-10-21 10:58:11 -0300

    add enable category use case

---
+++ src/Application/Finance.Application/UseCases/Category/EnableCategory/EnableCategoryHandler.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+
+namespace Finance.Application.UseCases.Category.EnableCategory
+{
+    public class EnableCategoryHandler : IEnableCategoryHandler
+    {
+        public Task<CategoryResponse> Handle(EnableCategoryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
