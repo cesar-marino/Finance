3126ad1239dda069f4e57521f1978b3b92e97e1c
César Marino <cesarmarino00@gmail.com>
2024-10-21 10:47:58 -0300

    ensure rethrow same exception that CommitAsync throws

--- src/Application/Finance.Application/UseCases/Category/DisableCategory/DisableCategoryHandler.cs
+++ src/Application/Finance.Application/UseCases/Category/DisableCategory/DisableCategoryHandler.cs
@@ -1,15 +1,19 @@
 ﻿using Finance.Application.UseCases.Category.Commons;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 
 namespace Finance.Application.UseCases.Category.DisableCategory
 {
-    public class DisableCategoryHandler(ICategoryRepository categoryRepository) : IDisableCategoryHandler
+    public class DisableCategoryHandler(
+        ICategoryRepository categoryRepository,
+        IUnitOfWork unitOfWork) : IDisableCategoryHandler
     {
         public async Task<CategoryResponse> Handle(DisableCategoryRequest request, CancellationToken cancellationToken)
         {
             var category = await categoryRepository.FindAsync(request.CategoryId, cancellationToken);
 
             await categoryRepository.UpdateAsync(category, cancellationToken);
+            await unitOfWork.CommitAsync(cancellationToken);
 
             throw new NotImplementedException();
         }
