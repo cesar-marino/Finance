bb550af9119c88e96939e97c0ca39e7ec81471a5
César Marino <cesarmarino00@gmail.com>
2024-10-21 10:00:51 -0300

    add category entity

---
+++ src/Domain/Finance.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,41 @@
+﻿using Finance.Domain.SeedWork;
+
+namespace Finance.Domain.Entities
+{
+    public class CategoryEntity : AggregateRoot
+    {
+        public bool Active { get; }
+        public string Name { get; }
+        public string? Icon { get; }
+        public string? Color { get; }
+
+        private readonly List<Guid> _subcategories = [];
+        public IReadOnlyList<Guid> Subcategories => _subcategories.AsReadOnly();
+
+        public CategoryEntity(string name, string? icon, string? color)
+        {
+            Active = true;
+            Name = name;
+            Icon = icon;
+            Color = color;
+            _subcategories = [];
+        }
+
+        public CategoryEntity(
+            Guid categoryId,
+            bool active,
+            string name,
+            string? icon,
+            string? color,
+            IReadOnlyList<Guid> subcategories,
+            DateTime createdAt,
+            DateTime updatedAt) : base(categoryId, createdAt, updatedAt)
+        {
+            Active = active;
+            Name = name;
+            Icon = icon;
+            Color = color;
+            _subcategories = subcategories.ToList();
+        }
+    }
+}
---
+++ src/Domain/Finance.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Finance.Domain.Entities;
+using Finance.Domain.SeedWork;
+
+namespace Finance.Domain.Repositories
+{
+    public interface ICategoryRepository : IRepository<CategoryEntity>
+    {
+    }
+}
