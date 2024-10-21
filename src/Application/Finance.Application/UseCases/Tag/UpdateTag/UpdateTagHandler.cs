137fb6501fcdda54134baf6b0d89bfba6e0b3c26
César Marino <cesarmarino00@gmail.com>
2024-10-21 16:50:05 -0300

    ensure rethrow same exception that FindAsync throws

--- src/Application/Finance.Application/UseCases/Tag/UpdateTag/UpdateTagHandler.cs
+++ src/Application/Finance.Application/UseCases/Tag/UpdateTag/UpdateTagHandler.cs
@@ -1,11 +1,17 @@
 ﻿using Finance.Application.UseCases.Tag.Commons;
+using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Tag.UpdateTag
 {
-    public class UpdateTagHandler : IUpdateTagHandler
+    public class UpdateTagHandler(ITagRepository tagRepository) : IUpdateTagHandler
     {
-        public Task<TagResponse> Handle(UpdateTagRequest request, CancellationToken cancellationToken)
+        public async Task<TagResponse> Handle(UpdateTagRequest request, CancellationToken cancellationToken)
         {
+            await tagRepository.FindAsync(
+                accountId: request.AccountId,
+                entityId: request.TagId,
+                cancellationToken);
+
             throw new NotImplementedException();
         }
     }
---
+++ test/Finance.Test.UnitTest/Application/UseCases/Tag/UpdateTag/UpdateTagHandlerTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using Finance.Application.UseCases.Tag.UpdateTag;
+using Finance.Test.UnitTest.Commons;
+
+namespace Finance.Test.UnitTest.Application.UseCases.Tag.UpdateTag
+{
+    public class UpdateTagHandlerTestFixture : FixtureBase
+    {
+        public UpdateTagRequest MakeUpdateTagRequest() => new(
+            accountId: Faker.Random.Guid(),
+            tagId: Faker.Random.Guid(),
+            name: Faker.Random.String(5));
+    }
+}
