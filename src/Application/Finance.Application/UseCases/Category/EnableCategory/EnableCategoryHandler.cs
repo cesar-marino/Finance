e2e1fd9248e7d4831bb18237cbc28c61f86f123e
César Marino <cesarmarino00@gmail.com>
2024-10-21 11:05:36 -0300

    ensure rethrow same exception that FindAsync throws

--- src/Application/Finance.Application/UseCases/Category/EnableCategory/EnableCategoryHandler.cs
+++ src/Application/Finance.Application/UseCases/Category/EnableCategory/EnableCategoryHandler.cs
@@ -1,11 +1,14 @@
 ﻿using Finance.Application.UseCases.Category.Commons;
+using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Category.EnableCategory
 {
-    public class EnableCategoryHandler : IEnableCategoryHandler
+    public class EnableCategoryHandler(ICategoryRepository categoryRepository) : IEnableCategoryHandler
     {
-        public Task<CategoryResponse> Handle(EnableCategoryRequest request, CancellationToken cancellationToken)
+        public async Task<CategoryResponse> Handle(EnableCategoryRequest request, CancellationToken cancellationToken)
         {
+            var category = await categoryRepository.FindAsync(request.CategoryId, cancellationToken);
+
             throw new NotImplementedException();
         }
     }
---
+++ test/Finance.Test.UnitTests/Application/UseCases/Category/EnableCategory/EnableCategoryHandlerTest.cs
@@ -0,0 +1,40 @@
+﻿using Finance.Application.UseCases.Category.EnableCategory;
+using Finance.Domain.Exceptions;
+using Finance.Domain.Repositories;
+using FluentAssertions;
+using Moq;
+
+namespace Finance.Test.UnitTests.Application.UseCases.Category.EnableCategory
+{
+    public class EnableCategoryHandlerTest : IClassFixture<EnableCategoryHandlerTestFixture>
+    {
+        private readonly EnableCategoryHandlerTestFixture _fixture;
+        private readonly EnableCategoryHandler _sut;
+        private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
+
+        public EnableCategoryHandlerTest(EnableCategoryHandlerTestFixture fixture)
+        {
+            _fixture = fixture;
+            _categoryRepositoryMock = new();
+
+            _sut = new(
+                categoryRepository: _categoryRepositoryMock.Object);
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatFindAsyncThrows))]
+        [Trait("Unit/UseCase", "Category - EnableCategory")]
+        public async Task ShouldRethrowSameExceptionThatFindAsyncThrows()
+        {
+            _categoryRepositoryMock
+                .Setup(x => x.FindAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new NotFoundException("Category"));
+
+            var request = _fixture.MakeEnableCategoryRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<NotFoundException>()
+                .Where(x => x.Code == "not-found")
+                .WithMessage("Category not found");
+        }
+    }
+}
