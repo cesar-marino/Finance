db9f4489922960ec77efa132837dd0da0872a4cd
César Marino <cesarmarino00@gmail.com>
2024-10-21 15:43:22 -0300

    add get tag use case

---
+++ src/Application/Finance.Application/UseCases/Tag/GetTag/GetTagHandler.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Application.UseCases.Tag.Commons;
+
+namespace Finance.Application.UseCases.Tag.GetTag
+{
+    public class GetTagHandler : IGetTagHandler
+    {
+        public Task<TagResponse> Handle(GetTagRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/Finance.Application/UseCases/Tag/GetTag/GetTagRequest.cs
@@ -0,0 +1,13 @@
+﻿using Finance.Application.UseCases.Tag.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Tag.GetTag
+{
+    public class GetTagRequest(
+        Guid accountId,
+        Guid tagId) : IRequest<TagResponse>
+    {
+        public Guid AccountId { get; } = accountId;
+        public Guid TagId { get; } = tagId;
+    }
+}
