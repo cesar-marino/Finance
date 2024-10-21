dc9562ed187cdcbba82659a832ab362dd7c94d5f
César Marino <cesarmarino00@gmail.com>
2024-10-21 11:23:19 -0300

    add update category use case

---
+++ src/Application/Finance.Application/UseCases/Category/UpdateCategory/UpdateCategoryHandler.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+
+namespace Finance.Application.UseCases.Category.UpdateCategory
+{
+    public class UpdateCategoryHandler : IUpdateCategoryHandler
+    {
+        public Task<CategoryResponse> Handle(UpdateCategoryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/Finance.Application/UseCases/Category/UpdateCategory/UpdateCategoryRequest.cs
@@ -0,0 +1,17 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Category.UpdateCategory
+{
+    public class UpdateCategoryRequest(
+        Guid categoryId,
+        string name,
+        string? icon,
+        string? color) : IRequest<CategoryResponse>
+    {
+        public Guid CategoryId { get; } = categoryId;
+        public string Name { get; } = name;
+        public string? Icon { get; } = icon;
+        public string? Color { get; } = color;
+    }
+}
