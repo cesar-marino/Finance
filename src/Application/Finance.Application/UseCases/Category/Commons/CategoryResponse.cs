a0972e6107c92dc6c28a8852dffaf4c986ac7a81
César Marino <cesarmarino00@gmail.com>
2024-10-21 10:06:31 -0300

    add create category use case

---
+++ src/Application/Finance.Application/UseCases/Category/Commons/CategoryResponse.cs
@@ -0,0 +1,31 @@
+﻿using Finance.Domain.Entities;
+
+namespace Finance.Application.UseCases.Category.Commons
+{
+    public class CategoryResponse(
+        Guid categoryId,
+        bool active,
+        string name,
+        string? icon,
+        string? color,
+        DateTime createdAt,
+        DateTime updatedAt)
+    {
+        public Guid CategoryId { get; } = categoryId;
+        public bool Active { get; } = active;
+        public string Name { get; } = name;
+        public string? Icon { get; } = icon;
+        public string? Color { get; } = color;
+        public DateTime CreatedAt { get; } = createdAt;
+        public DateTime UpdatedAt { get; } = updatedAt;
+
+        public static CategoryResponse FromEntity(CategoryEntity category) => new(
+                categoryId: category.Id,
+                active: category.Active,
+                name: category.Name,
+                icon: category.Icon,
+                color: category.Color,
+                createdAt: category.CreatedAt,
+                updatedAt: category.UpdatedAt);
+    }
+}
---
+++ src/Application/Finance.Application/UseCases/Category/CreateCategory/CreateCategoryRequest.cs
@@ -0,0 +1,15 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Category.CreateCategory
+{
+    public class CreateCategoryRequest(
+        string name,
+        string? icon,
+        string? color) : IRequest<CategoryResponse>
+    {
+        public string Name { get; } = name;
+        public string? Icon { get; } = icon;
+        public string? Color { get; } = color;
+    }
+}
