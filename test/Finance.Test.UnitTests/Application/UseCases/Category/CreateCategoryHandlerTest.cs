809435e8cd7843925cb8c263cb4c2ac5540ef04c
César Marino <cesarmarino00@gmail.com>
2024-10-21 10:17:48 -0300

    ensure rethrow same exception that CommitAsync throws

--- test/Finance.Test.UnitTests/Application/UseCases/Category/CreateCategoryHandlerTest.cs
+++ test/Finance.Test.UnitTests/Application/UseCases/Category/CreateCategoryHandlerTest.cs
@@ -2,6 +2,7 @@
 using Finance.Domain.Entities;
 using Finance.Domain.Exceptions;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 using FluentAssertions;
 using Moq;
 
@@ -12,14 +13,17 @@ namespace Finance.Test.UnitTests.Application.UseCases.Category
         private readonly CreateCategoryHandlerTestFixture _fixture;
         private readonly CreateCategoryHandler _sut;
         private readonly Mock<ICategoryRepository> _categoryRepositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
 
         public CreateCategoryHandlerTest(CreateCategoryHandlerTestFixture fixture)
         {
             _fixture = fixture;
             _categoryRepositoryMock = new();
+            _unitOfWorkMock = new();
 
             _sut = new(
-                categoryRepository: _categoryRepositoryMock.Object);
+                categoryRepository: _categoryRepositoryMock.Object,
+                unitOfWork: _unitOfWorkMock.Object);
         }
 
         [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatInsertAsyncThrows))]
@@ -28,6 +32,22 @@ namespace Finance.Test.UnitTests.Application.UseCases.Category
         {
             _categoryRepositoryMock
                 .Setup(x => x.InsertAsync(It.IsAny<CategoryEntity>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakeCreateCategoryRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatCommitAsyncThrows))]
+        [Trait("Unit/UseCase", "Category - CreateCategory")]
+        public async Task ShouldRethrowSameExceptionThatCommitAsyncThrows()
+        {
+            _unitOfWorkMock
+                .Setup(x => x.CommitAsync(It.IsAny<CancellationToken>()))
                 .ThrowsAsync(new UnexpectedException());
 
             var request = _fixture.MakeCreateCategoryRequest();
