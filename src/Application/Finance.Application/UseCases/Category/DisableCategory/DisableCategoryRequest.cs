a02c4c80d3f42b675d51211ed2ff66876f669cc9
César Marino <cesarmarino00@gmail.com>
2024-10-21 10:33:47 -0300

    add disable category use case

---
+++ src/Application/Finance.Application/UseCases/Category/DisableCategory/DisableCategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Category.DisableCategory
+{
+    public class DisableCategoryRequest(Guid categoryId) : IRequest<CategoryResponse>
+    {
+        public Guid CategoryId { get; } = categoryId;
+    }
+}
