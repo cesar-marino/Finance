2a8a90c5ecd84bd446739b5aa248d2253e395616
César Marino <cesarmarino00@gmail.com>
2024-10-23 16:54:06 -0300

    ensure rethrow same exception that UpdateAsync throws

--- src/Application/Finance.Application/UseCases/Category/UpdateCategory/UpdateCategoryHandler.cs
+++ src/Application/Finance.Application/UseCases/Category/UpdateCategory/UpdateCategoryHandler.cs
@@ -7,10 +7,12 @@ namespace Finance.Application.UseCases.Category.UpdateCategory
     {
         public async Task<CategoryResponse> Handle(UpdateCategoryRequest request, CancellationToken cancellationToken)
         {
-            await categoryRepository.FindAsync(
+            var category = await categoryRepository.FindAsync(
                 accountId: request.AccountId,
                 entityId: request.CategoryId,
                 cancellationToken);
+
+            await categoryRepository.UpdateAsync(category, cancellationToken);
 
             throw new NotImplementedException();
         }
