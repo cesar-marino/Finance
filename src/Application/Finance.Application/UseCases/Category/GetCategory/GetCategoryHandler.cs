460e542dd98f4f05e9e0e58c129a10355bf9ea5b
César Marino <cesarmarino00@gmail.com>
2024-10-23 10:54:37 -0300

    ensure rethrow same exception that FindAsync throws

--- src/Application/Finance.Application/UseCases/Category/GetCategory/GetCategoryHandler.cs
+++ src/Application/Finance.Application/UseCases/Category/GetCategory/GetCategoryHandler.cs
@@ -1,11 +1,17 @@
 ﻿using Finance.Application.UseCases.Category.Commons;
+using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Category.GetCategory
 {
-    public class GetCategoryHandler : IGetCategoryHandler
+    public class GetCategoryHandler(ICategoryRepository categoryRepository) : IGetCategoryHandler
     {
-        public Task<CategoryResponse> Handle(GetCategoryRequest request, CancellationToken cancellationToken)
+        public async Task<CategoryResponse> Handle(GetCategoryRequest request, CancellationToken cancellationToken)
         {
+            await categoryRepository.FindAsync(
+                accountId: request.AccountId,
+                entityId: request.CategoryId,
+                cancellationToken);
+
             throw new NotImplementedException();
         }
     }
---
+++ test/Finance.Test.UnitTest/Application/UseCases/Category/GetCategory/GetCategoryHandlerTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Application.UseCases.Category.GetCategory;
+using Finance.Test.UnitTest.Commons;
+
+namespace Finance.Test.UnitTest.Application.UseCases.Category.GetCategory
+{
+    public class GetCategoryHandlerTestFixture : FixtureBase
+    {
+        public GetCategoryRequest MakeGetCategoryRequest() => new(
+            accountId: Faker.Random.Guid(),
+            categoryId: Faker.Random.Guid());
+    }
+}
