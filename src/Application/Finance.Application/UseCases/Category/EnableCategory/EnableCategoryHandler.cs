25a327d0c8db127a849ad864fa44f4ec72857fa9
César Marino <cesarmarino00@gmail.com>
2024-10-23 10:16:11 -0300

    ensure rethrow same exception that FindAsync throws

---
+++ src/Application/Finance.Application/UseCases/Category/EnableCategory/EnableCategoryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+using Finance.Domain.Repositories;
+
+namespace Finance.Application.UseCases.Category.EnableCategory
+{
+    public class EnableCategoryHandler(ICategoryRepository categoryRepository) : IEnableCategoryHandler
+    {
+        public async Task<CategoryResponse> Handle(EnableCategoryRequest request, CancellationToken cancellationToken)
+        {
+            await categoryRepository.FindAsync(
+                accountId: request.AccountId,
+                entityId: request.CategoryId,
+                cancellationToken);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Finance.Test.UnitTest/Application/UseCases/Category/EnableCategory/EnableCategoryHandlerTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Application.UseCases.Category.EnableCategory;
+using Finance.Test.UnitTest.Commons;
+
+namespace Finance.Test.UnitTest.Application.UseCases.Category.EnableCategory
+{
+    public class EnableCategoryHandlerTestFixture : FixtureBase
+    {
+        public EnableCategoryRequest MakeEnableCategoryRequest() => new(
+            accountId: Faker.Random.Guid(),
+            categoryId: Faker.Random.Guid());
+    }
+}
