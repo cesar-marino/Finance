5afc43538ab7414f38291dbc3d3b5b4ff1061e1e
César Marino <cesarmarino00@hotmail.com>
2024-10-22 22:29:57 -0300

    ensure rethrow same exception that InsertAsync throws

---
+++ test/Finance.Test.UnitTest/Application/UseCases/Category/CreateCategory/CreateCategoryHandlerTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using Finance.Application.UseCases.Category.CreateCategory;
+using Finance.Test.UnitTest.Commons;
+
+namespace Finance.Test.UnitTest.Application.UseCases.Category.CreateCategory
+{
+    public class CreateCategoryHandlerTestFixture : FixtureBase
+    {
+        public CreateCategoryRequest MakeCreateCategoryRequest() => new(
+            accountId: Faker.Random.Guid(),
+            categoryType: Domain.Enums.CategoryType.Revenue,
+            name: Faker.Random.String(5),
+            icon: Faker.Random.String(5),
+            color: Faker.Random.String(5));
+    }
+}
