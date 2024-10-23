0641f5b5b7d0f24a543e2da3aed38b1d116cd39d
César Marino <cesarmarino00@gmail.com>
2024-10-23 10:40:56 -0300

    add get category use case

---
+++ src/Application/Finance.Application/UseCases/Category/GetCategory/GetCategoryHandler.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+
+namespace Finance.Application.UseCases.Category.GetCategory
+{
+    public class GetCategoryHandler : IGetCategoryHandler
+    {
+        public Task<CategoryResponse> Handle(GetCategoryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/Finance.Application/UseCases/Category/GetCategory/IGetCategoryHandler.cs
@@ -0,0 +1,9 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Category.GetCategory
+{
+    public interface IGetCategoryHandler : IRequestHandler<GetCategoryRequest, CategoryResponse>
+    {
+    }
+}
