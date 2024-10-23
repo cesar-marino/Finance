b77828f3dd5c76f71bcd30d422f9d31f5b42081f
César Marino <cesarmarino00@gmail.com>
2024-10-23 10:05:24 -0300

    add enable category use case

---
+++ src/Application/Finance.Application/UseCases/Category/EnableCategory/IEnableCategoryHandler.cs
@@ -0,0 +1,9 @@
+﻿using Finance.Application.UseCases.Category.Commons;
+using MediatR;
+
+namespace Finance.Application.UseCases.Category.EnableCategory
+{
+    public interface IEnableCategoryHandler : IRequestHandler<EnableCategoryRequest, CategoryResponse>
+    {
+    }
+}
