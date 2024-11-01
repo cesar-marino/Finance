96d30c27005e41f2a6aba54ebac8765f834db0ed
César Marino <cesarmarino00@gmail.com>
2024-11-01 14:44:30 -0300

    add limit endpoints

---
+++ src/Presentation/Finance.Presentation.API/Controllers/LimitController.cs
@@ -0,0 +1,52 @@
+﻿using Finance.Application.UseCases.Limit.Commons;
+using Finance.Application.UseCases.Limit.CreateLimit;
+using Finance.Application.UseCases.Limit.RemoveLimit;
+using Finance.Application.UseCases.Limit.UpdateLimit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Presentation.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LimitController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(LimitResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create(
+            [FromBody] CreateLimitRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(LimitResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Remove(
+            [FromBody] RemoveLimitRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(LimitResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(
+            [FromBody] UpdateLimitRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var response = await mediator.Send(request, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
