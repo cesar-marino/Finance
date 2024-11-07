037aabf80f32d462e29163ed89ebefd8a136b0be
César Marino <cesarmarino00@gmail.com>
2024-11-07 10:28:01 -0300

    add identity configuration

--- src/Application/Finance.Application/Services/IAccountService.cs
+++ src/Application/Finance.Application/Services/IAccountService.cs
@@ -2,6 +2,6 @@ namespace Finance.Application.Services
 {
     public interface IAccountService
     {
-        Task CreateAsync();
+        Task CreateAsync(Guid accountId, string email, string username, string passwrd);
     }
 }
