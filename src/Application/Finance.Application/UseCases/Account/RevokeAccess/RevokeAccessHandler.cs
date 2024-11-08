40bf042b46fb5f0068138b3689fd2b48547ac119
César Marino <cesarmarino00@gmail.com>
2024-11-08 16:40:32 -0300

    ensure rethrow same exception that CommitAsync throws

--- src/Application/Finance.Application/UseCases/Account/RevokeAccess/RevokeAccessHandler.cs
+++ src/Application/Finance.Application/UseCases/Account/RevokeAccess/RevokeAccessHandler.cs
@@ -1,15 +1,19 @@
 using Finance.Application.UseCases.Account.Commons;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 
 namespace Finance.Application.UseCases.Account.RevokeAccess
 {
-    public class RevokeAccessHandler(IAccountRepository accountRepository) : IRevokeAccessHandler
+    public class RevokeAccessHandler(
+        IAccountRepository accountRepository,
+        IUnitOfWork unitOfWork) : IRevokeAccessHandler
     {
         public async Task<AccountResponse> Handle(RevokeAccessRequest request, CancellationToken cancellationToken)
         {
             var account = await accountRepository.FindAsync(request.AccountId, cancellationToken);
 
             await accountRepository.UpdateAsync(account, cancellationToken);
+            await unitOfWork.CommitAsync(cancellationToken);
 
             throw new NotImplementedException();
         }
--- test/Finance.Test.UnitTest/Application/UseCases/Account/RevokeAccess/RevokeAccessHandlerTest.cs
+++ test/Finance.Test.UnitTest/Application/UseCases/Account/RevokeAccess/RevokeAccessHandlerTest.cs
@@ -2,6 +2,7 @@ using Finance.Application.UseCases.Account.RevokeAccess;
 using Finance.Domain.Entities;
 using Finance.Domain.Exceptions;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 using FluentAssertions;
 using Moq;
 
@@ -12,14 +13,17 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.RevokeAccess
         private readonly RevokeAccessHandlerTestFixture _fixture;
         private readonly RevokeAccessHandler _sut;
         private readonly Mock<IAccountRepository> _accountRepositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
 
         public RevokeAccessHandlerTest(RevokeAccessHandlerTestFixture fixture)
         {
             _fixture = fixture;
             _accountRepositoryMock = new();
+            _unitOfWorkMock = new();
 
             _sut = new(
-                accountRepository: _accountRepositoryMock.Object);
+                accountRepository: _accountRepositoryMock.Object,
+                unitOfWork: _unitOfWorkMock.Object);
         }
 
         [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatFindAsyncThrows))]
@@ -55,6 +59,29 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.RevokeAccess
                 .Setup(x => x.UpdateAsync(
                     It.IsAny<AccountEntity>(),
                     It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakeRevokeAccessRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatCommitAsyncThrows))]
+        [Trait("Unit/UseCase", "Account - RevokeAccess")]
+        public async Task ShouldRethrowSameExceptionThatCommitAsyncThrows()
+        {
+            var account = _fixture.MakeAccountEntity();
+            _accountRepositoryMock
+                .Setup(x => x.FindAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(account);
+
+            _unitOfWorkMock
+                .Setup(x => x.CommitAsync(It.IsAny<CancellationToken>()))
                 .ThrowsAsync(new UnexpectedException());
 
             var request = _fixture.MakeRevokeAccessRequest();
