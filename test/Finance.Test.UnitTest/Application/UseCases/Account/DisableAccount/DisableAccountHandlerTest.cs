3304277706f474fe81f363ab3c4b055ea4dbb3fa
César Marino <cesarmarino00@gmail.com>
2024-11-08 11:09:21 -0300

    ensure rethrow same exception that UpdateAsync throw

--- test/Finance.Test.UnitTest/Application/UseCases/Account/DisableAccount/DisableAccountHandlerTest.cs
+++ test/Finance.Test.UnitTest/Application/UseCases/Account/DisableAccount/DisableAccountHandlerTest.cs
@@ -1,4 +1,5 @@
 using Finance.Application.UseCases.Account.DisableAccount;
+using Finance.Domain.Entities;
 using Finance.Domain.Exceptions;
 using Finance.Domain.Repositories;
 using FluentAssertions;
@@ -36,6 +37,31 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.DisableAccount
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethorwSameExceptionThatUpdateAsyncThrows))]
+        [Trait("Unit/UseCase", "Account - DisableAccount")]
+        public async void ShouldRethorwSameExceptionThatUpdateAsyncThrows()
+        {
+            var account = _fixture.MakeAccountEntity();
+            _accountRepositoryMock
+                .Setup(x => x.FindAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(account);
+
+            _accountRepositoryMock
+                .Setup(x => x.UpdateAsync(
+                    It.IsAny<AccountEntity>(),
+                    It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakeDisableAccountRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
         }
     }
 }
