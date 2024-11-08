3f37a679d7dc0661e3083e72783b8389c75bcd40
César Marino <cesarmarino00@gmail.com>
2024-11-08 15:02:40 -0300

    ensure rethrow same exception that CommitAsync throws

--- src/Application/Finance.Application/UseCases/Account/EnableAccount/EnableAccountHandler.cs
+++ src/Application/Finance.Application/UseCases/Account/EnableAccount/EnableAccountHandler.cs
@@ -1,15 +1,19 @@
 using Finance.Application.UseCases.Account.Commons;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 
 namespace Finance.Application.UseCases.Account.EnableAccount
 {
-    public class EnableAccountHandler(IAccountRepository accountRepository) : IEnableAccountHandler
+    public class EnableAccountHandler(
+        IAccountRepository accountRepository,
+        IUnitOfWork unitOfWork) : IEnableAccountHandler
     {
         public async Task<AccountResponse> Handle(EnableAccountRequest request, CancellationToken cancellationToken)
         {
             var account = await accountRepository.FindAsync(request.AccountId, cancellationToken);
 
             await accountRepository.UpdateAsync(account, cancellationToken);
+            await unitOfWork.CommitAsync(cancellationToken);
 
             throw new NotImplementedException();
         }
--- test/Finance.Test.UnitTest/Application/UseCases/Account/EnableAccount/EnableAccountHandlerTest.cs
+++ test/Finance.Test.UnitTest/Application/UseCases/Account/EnableAccount/EnableAccountHandlerTest.cs
@@ -2,6 +2,7 @@ using Finance.Application.UseCases.Account.EnableAccount;
 using Finance.Domain.Entities;
 using Finance.Domain.Exceptions;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 using FluentAssertions;
 using Moq;
 
@@ -12,13 +13,17 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.EnableAccount
         private readonly EnableAccountHandlerTestFixture _fixture;
         private readonly EnableAccountHandler _sut;
         private readonly Mock<IAccountRepository> _accountRepositoryMock;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
 
         public EnableAccountHandlerTest(EnableAccountHandlerTestFixture fixture)
         {
             _fixture = fixture;
             _accountRepositoryMock = new();
+            _unitOfWorkMock = new();
 
-            _sut = new(accountRepository: _accountRepositoryMock.Object);
+            _sut = new(
+                accountRepository: _accountRepositoryMock.Object,
+                unitOfWork: _unitOfWorkMock.Object);
         }
 
         [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatFindAsyncThrows))]
@@ -54,6 +59,29 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.EnableAccount
                 .Setup(x => x.UpdateAsync(
                     It.IsAny<AccountEntity>(),
                     It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakeEnableAccountRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatCommitAsyncThrows))]
+        [Trait("Unit/UseCase", "Account - EnableAccount")]
+        public async Task ShouldRethrowSameExceptionThatCommitAsyncThrows()
+        {
+            var account = _fixture.MakeAccountEntity(active: false);
+            _accountRepositoryMock
+                .Setup(x => x.FindAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(account);
+
+            _unitOfWorkMock
+                .Setup(x => x.CommitAsync(It.IsAny<CancellationToken>()))
                 .ThrowsAsync(new UnexpectedException());
 
             var request = _fixture.MakeEnableAccountRequest();
