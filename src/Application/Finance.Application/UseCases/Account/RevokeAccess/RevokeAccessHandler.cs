824090a1dd50a975cd870e35f97623f73cd7b248
César Marino <cesarmarino00@gmail.com>
2024-11-08 16:38:20 -0300

    ensure rethrow same exception that UpdateAsync throws

--- src/Application/Finance.Application/UseCases/Account/RevokeAccess/RevokeAccessHandler.cs
+++ src/Application/Finance.Application/UseCases/Account/RevokeAccess/RevokeAccessHandler.cs
@@ -7,7 +7,9 @@ namespace Finance.Application.UseCases.Account.RevokeAccess
     {
         public async Task<AccountResponse> Handle(RevokeAccessRequest request, CancellationToken cancellationToken)
         {
-            await accountRepository.FindAsync(request.AccountId, cancellationToken);
+            var account = await accountRepository.FindAsync(request.AccountId, cancellationToken);
+
+            await accountRepository.UpdateAsync(account, cancellationToken);
 
             throw new NotImplementedException();
         }
