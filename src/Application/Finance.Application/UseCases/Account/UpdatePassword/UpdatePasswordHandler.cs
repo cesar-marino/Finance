2785327517873bd6c24c8c3eadb2b47f9da6b74f
César Marino <cesarmarino00@gmail.com>
2024-11-12 09:48:46 -0300

    ensure rethrow same exception that VerifyAsync throws

--- src/Application/Finance.Application/UseCases/Account/UpdatePassword/UpdatePasswordHandler.cs
+++ src/Application/Finance.Application/UseCases/Account/UpdatePassword/UpdatePasswordHandler.cs
@@ -1,13 +1,17 @@
+using Finance.Application.Services;
 using Finance.Application.UseCases.Account.Commons;
 using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Account.UpdatePassword
 {
-    public class UpdatePasswordHandler(IAccountRepository accountRepository) : IUpdatePasswordHandler
+    public class UpdatePasswordHandler(
+        IAccountRepository accountRepository,
+        IEncryptionService encryptionService) : IUpdatePasswordHandler
     {
         public async Task<AccountResponse> Handle(UpdatePasswordRequest request, CancellationToken cancellationToken)
         {
-            await accountRepository.FindAsync(request.AccountId, cancellationToken);
+            var account = await accountRepository.FindAsync(request.AccountId, cancellationToken);
+            await encryptionService.VerifyAsync(request.CurrentPassword, account.Password, cancellationToken);
 
             throw new NotImplementedException();
         }
--- test/Finance.Test.UnitTest/Application/UseCases/Account/UpdatePassword/UpdatePasswordHandlerTest.cs
+++ test/Finance.Test.UnitTest/Application/UseCases/Account/UpdatePassword/UpdatePasswordHandlerTest.cs
@@ -1,3 +1,4 @@
+using Finance.Application.Services;
 using Finance.Application.UseCases.Account.UpdatePassword;
 using Finance.Domain.Exceptions;
 using Finance.Domain.Repositories;
@@ -11,13 +12,17 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.UpdatePassword
         private readonly UpdatePasswordHandlerTestFixture _fixture;
         private readonly UpdatePasswordHandler _sut;
         private readonly Mock<IAccountRepository> _accountRepositoryMock;
+        private readonly Mock<IEncryptionService> _encryptionServiceMock;
 
         public UpdatePasswordHandlerTest(UpdatePasswordHandlerTestFixture fixture)
         {
             _fixture = fixture;
             _accountRepositoryMock = new();
+            _encryptionServiceMock = new();
+
             _sut = new(
-                accountRepository: _accountRepositoryMock.Object);
+                accountRepository: _accountRepositoryMock.Object,
+                encryptionService: _encryptionServiceMock.Object);
         }
 
         [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatFindAsyncThrows))]
@@ -36,6 +41,32 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.UpdatePassword
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatVerifyAsyncThrows))]
+        [Trait("Unit/UseCase", "Account - UpdatePassword")]
+        public async void ShouldRethrowSameExceptionThatVerifyAsyncThrows()
+        {
+            var account = _fixture.MakeAccountEntity();
+            _accountRepositoryMock
+                .Setup(x => x.FindAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(account);
+
+            _encryptionServiceMock
+                .Setup(x => x.VerifyAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new UnexpectedException());
+
+            var request = _fixture.MakeUpdatePasswordRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
         }
     }
 }
