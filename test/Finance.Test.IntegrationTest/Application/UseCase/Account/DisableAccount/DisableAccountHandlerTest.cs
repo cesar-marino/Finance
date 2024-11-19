3e511579ba4c5211c151bdda52f6c0bd178acc5e
César Marino <cesarmarino00@gmail.com>
2024-11-19 16:31:34 -0300

    ensure throw UnexpectedException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/DisableAccount/DisableAccountHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/DisableAccount/DisableAccountHandlerTest.cs
@@ -2,6 +2,7 @@ using Finance.Application.UseCases.Account.DisableAccount;
 using Finance.Domain.Exceptions;
 using Finance.Infrastructure.Database.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Test.IntegrationTest.Application.UseCase.Account.DisableAccount
 {
@@ -29,6 +30,31 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.DisableAccoun
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldThrowUnexpectedException))]
+        [Trait("Integration/UseCase", "Account - DisableAccount")]
+        public async Task ShouldThrowUnexpectedException()
+        {
+            var account = _fixture.MakeAccountModel();
+            var context = _fixture.MakeFinanceContext();
+
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var repository = new AccountRepository(context);
+
+            var sut = new DisableAccountHandler(accountRepository: repository, unitOfWork: context);
+
+            await context.DisposeAsync();
+
+            var request = _fixture.MakeDisableAccountRequest(accountId: account.AccountId);
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
         }
     }
 }
