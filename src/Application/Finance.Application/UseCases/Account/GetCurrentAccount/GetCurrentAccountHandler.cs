47093c5033cc3b4bcfc5f17be10df6eee0e6e120
César Marino <cesarmarino00@gmail.com>
2024-11-19 10:17:07 -0300

    ensure rethrow same exception that FindByUsernameAsync throws

--- src/Application/Finance.Application/UseCases/Account/GetCurrentAccount/GetCurrentAccountHandler.cs
+++ src/Application/Finance.Application/UseCases/Account/GetCurrentAccount/GetCurrentAccountHandler.cs
@@ -1,13 +1,17 @@
 using Finance.Application.Services;
 using Finance.Application.UseCases.Account.Commons;
+using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Account.GetCurrentAccount
 {
-    public class GetCurrentAccountHandler(ITokenService tokenService) : IGetCurrentAccountHandler
+    public class GetCurrentAccountHandler(
+        ITokenService tokenService,
+        IAccountRepository accountRepository) : IGetCurrentAccountHandler
     {
         public async Task<AccountResponse> Handle(GetCurrentAccountRequest request, CancellationToken cancellationToken)
         {
-            await tokenService.GetUsernameFromTokenAsync(request.AccessToken, cancellationToken);
+            var username = await tokenService.GetUsernameFromTokenAsync(request.AccessToken, cancellationToken);
+            await accountRepository.FindByUsernameAsync(username, cancellationToken);
 
             throw new NotImplementedException();
         }
--- test/Finance.Test.UnitTest/Application/UseCases/Account/GetCurrentAccount/GetCurrentAccountHandlerTest.cs
+++ test/Finance.Test.UnitTest/Application/UseCases/Account/GetCurrentAccount/GetCurrentAccountHandlerTest.cs
@@ -1,6 +1,7 @@
 using Finance.Application.Services;
 using Finance.Application.UseCases.Account.GetCurrentAccount;
 using Finance.Domain.Exceptions;
+using Finance.Domain.Repositories;
 using FluentAssertions;
 using Moq;
 
@@ -11,19 +12,22 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.GetCurrentAccount
         private readonly GetCurrentAccountHandlerTestFixture _fixture;
         private readonly GetCurrentAccountHandler _sut;
         private readonly Mock<ITokenService> _tokenServiceMock;
+        private readonly Mock<IAccountRepository> _accountRepositoryMock;
 
         public GetCurrentAccountHandlerTest(GetCurrentAccountHandlerTestFixture fixture)
         {
             _fixture = fixture;
             _tokenServiceMock = new();
+            _accountRepositoryMock = new();
 
             _sut = new(
-                tokenService: _tokenServiceMock.Object);
+                tokenService: _tokenServiceMock.Object,
+                accountRepository: _accountRepositoryMock.Object);
         }
 
-        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatGetUsernameFromTokenAsync))]
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatGetUsernameFromTokenAsyncThrows))]
         [Trait("Unit/UseCase", "Account - GetCurrentAccount")]
-        public async Task ShouldRethrowSameExceptionThatGetUsernameFromTokenAsync()
+        public async Task ShouldRethrowSameExceptionThatGetUsernameFromTokenAsyncThrows()
         {
             _tokenServiceMock
                 .Setup(x => x.GetUsernameFromTokenAsync(
@@ -37,6 +41,31 @@ namespace Finance.Test.UnitTest.Application.UseCases.Account.GetCurrentAccount
             await act.Should().ThrowExactlyAsync<UnexpectedException>()
                 .Where(x => x.Code == "unexpected")
                 .WithMessage("An unexpected error occurred");
+        }
+
+        [Fact(DisplayName = nameof(ShouldRethrowSameExceptionThatFindByUsernameAsyncThrows))]
+        [Trait("Unit/UseCase", "Account - GetCurrentAccount")]
+        public async Task ShouldRethrowSameExceptionThatFindByUsernameAsyncThrows()
+        {
+            var username = _fixture.Faker.Internet.UserName();
+            _tokenServiceMock
+                .Setup(x => x.GetUsernameFromTokenAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(username);
+
+            _accountRepositoryMock
+                .Setup(x => x.FindByUsernameAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new NotFoundException("Account"));
+
+            var request = _fixture.MakeGetCurrentAccountRequest();
+            var act = () => _sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<NotFoundException>()
+                .Where(x => x.Code == "not-found")
+                .WithMessage("Account not found");
         }
     }
 }
