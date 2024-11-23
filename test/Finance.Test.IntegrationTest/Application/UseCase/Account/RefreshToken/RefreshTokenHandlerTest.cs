e1ce916606b1ac71a8c0418654e2b5f0eabd428d
César Marino <cesarmarino00@gmail.com>
2024-11-23 13:02:59 -0300

    ensure throw UnauthorizedException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/RefreshToken/RefreshTokenHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/RefreshToken/RefreshTokenHandlerTest.cs
@@ -3,6 +3,7 @@ using Finance.Domain.Exceptions;
 using Finance.Infrastructure.Database.Repositories;
 using Finance.Infrastructure.Services.Token;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RefreshToken
 {
@@ -53,6 +54,36 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RefreshToken
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldThrowUnauthorizedException))]
+        [Trait("Integration/UseCase", "Account - RefreshToken")]
+        public async Task ShouldThrowUnauthorizedException()
+        {
+            var tokenService = new JwtBearerAdapter(_fixture.MakeConfiguration());
+            var account = _fixture.MakeAccountModel();
+            var token = await tokenService.GenerateAccessTokenAsync(account.ToEntity());
+            account.AccessTokenValue = token.Value;
+            account.AccessTokenExpiresIn = token.ExpiresIn;
+
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new RefreshTokenHandler(
+                tokenService: tokenService,
+                accountRepository: repository,
+                unitOfWork: context);
+
+            var request = _fixture.MakeRefreshTokenRequest(accessToken: token.Value);
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnauthorizedException>()
+                .Where(x => x.Code == "unauthorized")
+                .WithMessage("Unauthorized access");
         }
     }
 }
