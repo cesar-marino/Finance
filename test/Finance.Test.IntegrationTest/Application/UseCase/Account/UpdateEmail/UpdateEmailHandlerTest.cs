2388e42303687674f04f31371932638ae31f4c37
César Marino <cesarmarino00@gmail.com>
2024-11-23 14:14:23 -0300

    ensure return the correct response if email is successfully updated

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/UpdateEmail/UpdateEmailHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/UpdateEmail/UpdateEmailHandlerTest.cs
@@ -61,11 +61,6 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.UpdateEmail
             var context = _fixture.MakeFinanceContext();
             var repository = new AccountRepository(context);
 
-            var account = _fixture.MakeAccountModel();
-            var trackingInfo = await context.Accounts.AddAsync(account);
-            await context.SaveChangesAsync();
-            trackingInfo.State = EntityState.Detached;
-
             var sut = new UpdateEmailHandler(accountRepository: repository, unitOfWork: context);
 
             await context.DisposeAsync();
@@ -76,6 +71,44 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.UpdateEmail
             await act.Should().ThrowExactlyAsync<UnexpectedException>()
                 .Where(x => x.Code == "unexpected")
                 .WithMessage("An unexpected error occurred");
+        }
+
+        [Fact(DisplayName = nameof(ShouldReturnTheCorrectResponseIfEmailIsSuccessfullyUpdated))]
+        [Trait("INtegration/UseCase", "Account - UpdateEmail")]
+        public async Task ShouldReturnTheCorrectResponseIfEmailIsSuccessfullyUpdated()
+        {
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var account = _fixture.MakeAccountModel();
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new UpdateEmailHandler(accountRepository: repository, unitOfWork: context);
+
+            var request = _fixture.MakeUpdateEmailRequest(accountId: account.AccountId);
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            var accountDb = await context.Accounts.FirstOrDefaultAsync(x => x.AccountId == response.AccountId);
+
+            accountDb.Should().NotBeNull();
+            accountDb?.AccessTokenExpiresIn.Should().Be(response.AccessToken?.ExpiresIn);
+            accountDb?.AccessTokenValue.Should().Be(response.AccessToken?.Value);
+            accountDb?.AccountId.Should().Be(response.AccountId);
+            accountDb?.Active.Should().Be(response.Active);
+            accountDb?.Active.Should().Be(response.Active);
+            accountDb?.CreatedAt.Should().Be(response.CreatdAt);
+            accountDb?.Email.Should().Be(response.Email);
+            accountDb?.Email.Should().Be(request.Email);
+            accountDb?.EmailConfirmed.Should().Be(response.EmailConfirmed);
+            accountDb?.Phone.Should().Be(response.Phone);
+            accountDb?.PhoneConfirmed.Should().Be(response.PhoneConfirmed);
+            accountDb?.RefreshTokenExpiresIn.Should().Be(response.RefreshToken?.ExpiresIn);
+            accountDb?.RefreshTokenValue.Should().Be(response.RefreshToken?.Value);
+            accountDb?.Role.Should().Be(response.Role.ToString());
+            accountDb?.UpdatedAt.Should().Be(response.UpdatedAt);
+            accountDb?.Username.Should().Be(response.Username);
         }
     }
 }
