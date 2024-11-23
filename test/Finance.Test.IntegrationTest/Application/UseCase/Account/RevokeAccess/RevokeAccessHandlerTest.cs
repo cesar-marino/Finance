e93f35643c2fdaed7bfa5e23a8618c3507df1d9b
César Marino <cesarmarino00@gmail.com>
2024-11-23 13:36:47 -0300

    ensure return the correct response if access is successfully revoked

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/RevokeAccess/RevokeAccessHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/RevokeAccess/RevokeAccessHandlerTest.cs
@@ -50,5 +50,43 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RevokeAccess
                 .Where(x => x.Code == "unexpected")
                 .WithMessage("An unexpected error occurred");
         }
+
+        [Fact(DisplayName = nameof(ShouldReturnTheCorrectResponseIfAccessIsSuccessfullyRevoked))]
+        [Trait("Integration/UseCase", "Account - RevokeAccess")]
+        public async Task ShouldReturnTheCorrectResponseIfAccessIsSuccessfullyRevoked()
+        {
+            var account = _fixture.MakeAccountModel();
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new RevokeAccessHandler(accountRepository: repository, unitOfWork: context);
+            var request = _fixture.MakeRevokeAccessRequest(accountId: account.AccountId);
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            var accountDb = await context.Accounts.FirstOrDefaultAsync(x => x.AccountId == response.AccountId);
+            accountDb.Should().NotBeNull();
+            accountDb?.AccessTokenExpiresIn.Should().Be(response.AccessToken?.ExpiresIn);
+            accountDb?.AccessTokenExpiresIn.Should().BeNull();
+            accountDb?.AccessTokenValue.Should().Be(response.AccessToken?.Value);
+            accountDb?.AccessTokenValue.Should().BeNull();
+            accountDb?.AccountId.Should().Be(response.AccountId);
+            accountDb?.Active.Should().Be(response.Active);
+            accountDb?.CreatedAt.Should().Be(response.CreatdAt);
+            accountDb?.Email.Should().Be(response.Email);
+            accountDb?.EmailConfirmed.Should().Be(response.EmailConfirmed);
+            accountDb?.Phone.Should().Be(response.Phone);
+            accountDb?.PhoneConfirmed.Should().Be(response.PhoneConfirmed);
+            accountDb?.RefreshTokenExpiresIn.Should().Be(response.RefreshToken?.ExpiresIn);
+            accountDb?.RefreshTokenExpiresIn.Should().BeNull();
+            accountDb?.RefreshTokenValue.Should().Be(response.RefreshToken?.Value);
+            accountDb?.RefreshTokenValue.Should().BeNull();
+            accountDb?.Role.Should().Be(response.Role.ToString());
+            accountDb?.UpdatedAt.Should().Be(response.UpdatedAt);
+            accountDb?.Username.Should().Be(response.Username);
+        }
     }
 }
