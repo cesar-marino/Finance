b7750f7f433f213e603d3ef6311eea2131bd2ad0
César Marino <cesarmarino00@gmail.com>
2024-11-23 12:35:13 -0300

    ensure return the correct response if account is found

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/GetCurrentAccount/GetCurrentAccountHnadlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/GetCurrentAccount/GetCurrentAccountHnadlerTest.cs
@@ -3,6 +3,7 @@ using Finance.Domain.Exceptions;
 using Finance.Infrastructure.Database.Repositories;
 using Finance.Infrastructure.Services.Token;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Test.IntegrationTest.Application.UseCase.Account.GetCurrentAccount
 {
@@ -47,6 +48,46 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.GetCurrentAcc
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldReturnTheCorrectResponseIfAccountIsFound))]
+        [Trait("Integration/UseCase", "Account - GetCurrentAccount")]
+        public async Task ShouldReturnTheCorrectResponseIfAccountIsFound()
+        {
+            var tokenService = new JwtBearerAdapter(_fixture.MakeConfiguration());
+            var account = _fixture.MakeAccountModel();
+            var token = await tokenService.GenerateAccessTokenAsync(account.ToEntity());
+            account.AccessTokenValue = token.Value;
+            account.AccessTokenExpiresIn = token.ExpiresIn;
+
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new GetCurrentAccountHandler(tokenService, repository);
+
+            var request = _fixture.MakeGetCurrentAccountRequest(tokenValue: token.Value);
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            var accountDb = await context.Accounts.FirstOrDefaultAsync(x => x.AccountId == response.AccountId);
+            accountDb.Should().NotBeNull();
+            accountDb?.AccessTokenExpiresIn.Should().Be(response.AccessToken?.ExpiresIn);
+            accountDb?.AccessTokenValue.Should().Be(response.AccessToken?.Value);
+            accountDb?.AccountId.Should().Be(response.AccountId);
+            accountDb?.Active.Should().Be(response.Active);
+            accountDb?.CreatedAt.Should().Be(response.CreatdAt);
+            accountDb?.Email.Should().Be(response.Email);
+            accountDb?.EmailConfirmed.Should().Be(response.EmailConfirmed);
+            accountDb?.Phone.Should().Be(response.Phone);
+            accountDb?.PhoneConfirmed.Should().Be(response.PhoneConfirmed);
+            accountDb?.RefreshTokenExpiresIn.Should().Be(response.RefreshToken?.ExpiresIn);
+            accountDb?.RefreshTokenValue.Should().Be(response.RefreshToken?.Value);
+            accountDb?.Role.Should().Be(response.Role.ToString());
+            accountDb?.UpdatedAt.Should().Be(response.UpdatedAt);
+            accountDb?.Username.Should().Be(response.Username);
         }
     }
 }
