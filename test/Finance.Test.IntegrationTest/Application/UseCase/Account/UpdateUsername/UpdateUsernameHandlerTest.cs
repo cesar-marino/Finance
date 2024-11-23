00b87d9496309b3fee0588d5ee0e32b0def3efe1
César Marino <cesarmarino00@gmail.com>
2024-11-23 15:40:41 -0300

    ensure throw UnexpectedException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/UpdateUsername/UpdateUsernameHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/UpdateUsername/UpdateUsernameHandlerTest.cs
@@ -48,5 +48,29 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.UpdateUsernam
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
         }
+
+        [Fact(DisplayName = nameof(ShouldThrowUnexpectedException))]
+        [Trait("Integration/UseCase", "Account - UpdateUsername")]
+        public async Task ShouldThrowUnexpectedException()
+        {
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var account = _fixture.MakeAccountModel();
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new UpdateUsernameHandler(accountRepository: repository, unitOfWork: context);
+
+            await context.DisposeAsync();
+
+            var request = _fixture.MakeUpdateUsernameRequest(accountId: account.AccountId);
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
+        }
     }
 }
