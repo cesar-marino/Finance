c45b36d645fa716f391d7a2cb314a51bffaf2b72
César Marino <cesarmarino00@gmail.com>
2024-11-23 13:32:14 -0300

    ensure throw UnexpectedException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/RevokeAccess/RevokeAccessHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/RevokeAccess/RevokeAccessHandlerTest.cs
@@ -2,6 +2,7 @@ using Finance.Application.UseCases.Account.RevokeAccess;
 using Finance.Domain.Exceptions;
 using Finance.Infrastructure.Database.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RevokeAccess
 {
@@ -24,6 +25,30 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RevokeAccess
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldThrowUnexpectedException))]
+        [Trait("Integration/UseCase", "Account - RevokeAccess")]
+        public async Task ShouldThrowUnexpectedException()
+        {
+            var account = _fixture.MakeAccountModel();
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new RevokeAccessHandler(accountRepository: repository, unitOfWork: context);
+
+            await context.DisposeAsync();
+
+            var request = _fixture.MakeRevokeAccessRequest();
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
         }
     }
 }
