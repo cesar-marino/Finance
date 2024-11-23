32e5cec4b7ce153c8935feeeebcc186aae5d2877
César Marino <cesarmarino00@gmail.com>
2024-11-23 15:36:55 -0300

    ensure throw NotFoundException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/UpdateUsername/UpdateUsernameHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/UpdateUsername/UpdateUsernameHandlerTest.cs
@@ -31,5 +31,22 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.UpdateUsernam
                 .Where(x => x.Code == "username-in-use")
                 .WithMessage("Username is already in use");
         }
+
+        [Fact(DisplayName = nameof(ShouldThrowNotFoundException))]
+        [Trait("Integration/UseCase", "Account - UpdateUsername")]
+        public async Task ShouldThrowNotFoundException()
+        {
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var sut = new UpdateUsernameHandler(accountRepository: repository, unitOfWork: context);
+
+            var request = _fixture.MakeUpdateUsernameRequest();
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<NotFoundException>()
+                .Where(x => x.Code == "not-found")
+                .WithMessage("Account not found");
+        }
     }
 }
