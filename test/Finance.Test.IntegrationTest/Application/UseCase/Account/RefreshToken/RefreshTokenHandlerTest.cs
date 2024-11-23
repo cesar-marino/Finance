c8e2d14d631e01b3ea267622476aad42bd49270a
César Marino <cesarmarino00@gmail.com>
2024-11-23 12:58:22 -0300

    ensure throw NotFoundException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/RefreshToken/RefreshTokenHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/RefreshToken/RefreshTokenHandlerTest.cs
@@ -30,5 +30,29 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RefreshToken
                 .Where(x => x.Code == "invalid-token")
                 .WithMessage("Token is invalid");
         }
+
+        [Fact(DisplayName = nameof(ShouldThrowNotFoundException))]
+        [Trait("Integration/UseCase", "Account - RefreshToken")]
+        public async Task ShouldThrowNotFoundException()
+        {
+            var tokenService = new JwtBearerAdapter(_fixture.MakeConfiguration());
+            var account = _fixture.MakeAccountModel();
+            var token = await tokenService.GenerateAccessTokenAsync(account.ToEntity());
+
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var sut = new RefreshTokenHandler(
+                tokenService: tokenService,
+                accountRepository: repository,
+                unitOfWork: context);
+
+            var request = _fixture.MakeRefreshTokenRequest(accessToken: token.Value);
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<NotFoundException>()
+                .Where(x => x.Code == "not-found")
+                .WithMessage("Account not found");
+        }
     }
 }
