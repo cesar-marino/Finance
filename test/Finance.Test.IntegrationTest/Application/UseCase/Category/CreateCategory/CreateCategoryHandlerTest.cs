bd4ad964724ab0873a8cbc0592aeac54cfa61e58
César Marino <cesarmarino00@gmail.com>
2024-11-23 16:10:19 -0300

    ensure throw UnexpectedException

--- test/Finance.Test.IntegrationTest/Application/UseCase/Category/CreateCategory/CreateCategoryHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Category/CreateCategory/CreateCategoryHandlerTest.cs
@@ -2,6 +2,7 @@ using Finance.Application.UseCases.Category.CreateCategory;
 using Finance.Domain.Exceptions;
 using Finance.Infrastructure.Database.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Test.IntegrationTest.Application.UseCase.Category.CreateCategory
 {
@@ -24,6 +25,30 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Category.CreateCatego
             await act.Should().ThrowExactlyAsync<NotFoundException>()
                 .Where(x => x.Code == "not-found")
                 .WithMessage("Account not found");
+        }
+
+        [Fact(DisplayName = nameof(ShouldThrowUnexpectedException))]
+        [Trait("Integration/UseCase", "Category - CreateCategory")]
+        public async Task ShouldThrowUnexpectedException()
+        {
+            var context = _fixture.MakeFinanceContext();
+            var repository = new CategoryRepository(context);
+
+            var account = _fixture.MakeAccountModel();
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new CreateCategoryHandler(categoryRepository: repository, unitOfWork: context);
+
+            await context.DisposeAsync();
+
+            var request = _fixture.MakeCreateCategoryRequest(accountId: account.AccountId);
+            var act = () => sut.Handle(request, _fixture.CancellationToken);
+
+            await act.Should().ThrowExactlyAsync<UnexpectedException>()
+                .Where(x => x.Code == "unexpected")
+                .WithMessage("An unexpected error occurred");
         }
     }
 }
