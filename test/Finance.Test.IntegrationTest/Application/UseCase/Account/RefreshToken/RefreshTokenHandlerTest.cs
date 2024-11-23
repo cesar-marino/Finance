c765110ba0eab9361c09459635b56a05ba3a5ff5
César Marino <cesarmarino00@gmail.com>
2024-11-23 13:21:05 -0300

    ensure return the correct response if tokens are successfully refreshed

--- test/Finance.Test.IntegrationTest/Application/UseCase/Account/RefreshToken/RefreshTokenHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Account/RefreshToken/RefreshTokenHandlerTest.cs
@@ -117,5 +117,48 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Account.RefreshToken
                 .Where(x => x.Code == "unexpected")
                 .WithMessage("An unexpected error occurred");
         }
+
+        [Fact(DisplayName = nameof(ShouldReturnTheCorrectResponseIfTokensAreSuccessfullyRefreshed))]
+        [Trait("Integration/UseCase", "Account - RefreshToken")]
+        public async Task ShouldReturnTheCorrectResponseIfTokensAreSuccessfullyRefreshed()
+        {
+            var tokenService = new JwtBearerAdapter(_fixture.MakeConfiguration());
+            var account = _fixture.MakeAccountModel();
+            var token = await tokenService.GenerateAccessTokenAsync(account.ToEntity());
+            account.AccessTokenValue = token.Value;
+            account.AccessTokenExpiresIn = token.ExpiresIn;
+
+            var context = _fixture.MakeFinanceContext();
+            var repository = new AccountRepository(context);
+
+            var trackingInfo = await context.Accounts.AddAsync(account);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new RefreshTokenHandler(
+                tokenService: tokenService,
+                accountRepository: repository,
+                unitOfWork: context);
+
+            var request = _fixture.MakeRefreshTokenRequest(accessToken: token.Value, refreshToken: account.RefreshTokenValue);
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            var accountDb = await context.Accounts.FirstOrDefaultAsync(x => x.AccountId == response.AccountId);
+            accountDb.Should().NotBeNull();
+            accountDb?.AccessTokenExpiresIn.Should().Be(response.AccessToken?.ExpiresIn);
+            accountDb?.AccessTokenValue.Should().Be(response.AccessToken?.Value);
+            accountDb?.AccountId.Should().Be(response.AccountId);
+            accountDb?.Active.Should().Be(response.Active);
+            accountDb?.CreatedAt.Should().Be(response.CreatdAt);
+            accountDb?.Email.Should().Be(response.Email);
+            accountDb?.EmailConfirmed.Should().Be(response.EmailConfirmed);
+            accountDb?.Phone.Should().Be(response.Phone);
+            accountDb?.PhoneConfirmed.Should().Be(response.PhoneConfirmed);
+            accountDb?.RefreshTokenExpiresIn.Should().Be(response.RefreshToken?.ExpiresIn);
+            accountDb?.RefreshTokenValue.Should().Be(response.RefreshToken?.Value);
+            accountDb?.Role.Should().Be(response.Role.ToString());
+            accountDb?.UpdatedAt.Should().Be(response.UpdatedAt);
+            accountDb?.Username.Should().Be(response.Username);
+        }
     }
 }
