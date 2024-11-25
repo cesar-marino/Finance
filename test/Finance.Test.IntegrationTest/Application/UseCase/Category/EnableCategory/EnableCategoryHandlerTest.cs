728ca4d992fd5d696535445ecc77ee30091b29c1
César Marino <cesarmarino00@gmail.com>
2024-11-25 15:11:59 -0300

    ensure return the correct response if category is successfully enabled

--- test/Finance.Test.IntegrationTest/Application/UseCase/Category/EnableCategory/EnableCategoryHandlerTest.cs
+++ test/Finance.Test.IntegrationTest/Application/UseCase/Category/EnableCategory/EnableCategoryHandlerTest.cs
@@ -2,6 +2,7 @@ using Finance.Application.UseCases.Category.EnableCategory;
 using Finance.Domain.Exceptions;
 using Finance.Infrastructure.Database.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Test.IntegrationTest.Application.UseCase.Category.EnableCategory
 {
@@ -43,6 +44,35 @@ namespace Finance.Test.IntegrationTest.Application.UseCase.Category.EnableCatego
             await act.Should().ThrowExactlyAsync<UnexpectedException>()
                 .Where(x => x.Code == "unexpected")
                 .WithMessage("An unexpected error occurred");
+        }
+
+        [Fact(DisplayName = nameof(ShouldReturnTheCorrectResponseIfCategoryIsSuccessfullyEnabled))]
+        [Trait("Integration/UseCase", "Category - EnableCategory")]
+        public async Task ShouldReturnTheCorrectResponseIfCategoryIsSuccessfullyEnabled()
+        {
+            var context = _fixture.MakeFinanceContext();
+            var repository = new CategoryRepository(context);
+
+            var category = _fixture.MakeCategoryModel(active: false);
+            var trackingInfo = await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+            trackingInfo.State = EntityState.Detached;
+
+            var sut = new EnableCategoryHandler(categoryRepository: repository, unitOfWork: context);
+
+            var request = _fixture.MakeEnableCategoryRequest(accountId: category.AccountId, categoryId: category.CategoryId);
+            var response = await sut.Handle(request, _fixture.CancellationToken);
+
+            var categoryDb = await context.Categories.FirstOrDefaultAsync(x => x.CategoryId == category.CategoryId);
+            categoryDb?.AccountId.Should().Be(response.AccountId);
+            categoryDb?.Active.Should().BeTrue();
+            categoryDb?.CategoryId.Should().Be(response.CategoryId);
+            categoryDb?.CategoryType.Should().Be(response.CategoryType);
+            categoryDb?.Color.Should().Be(response.Color);
+            categoryDb?.CreatedAt.Should().Be(response.CreatedAt);
+            categoryDb?.Icon.Should().Be(response.Icon);
+            categoryDb?.Name.Should().Be(response.Name);
+            categoryDb?.SuperCategoryId.Should().Be(response.SuperCategory?.Id);
         }
     }
 }
