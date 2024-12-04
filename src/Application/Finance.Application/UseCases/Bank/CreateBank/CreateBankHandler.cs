5d035a13898a0dc9777186f0c0ec96ae86c5ef2e
César Marino <cesarmarino00@gmail.com>
2024-12-04 16:30:07 -0300

    ensure rethrow same exception that CommitAsync throws

--- src/Application/Finance.Application/UseCases/Bank/CreateBank/CreateBankHandler.cs
+++ src/Application/Finance.Application/UseCases/Bank/CreateBank/CreateBankHandler.cs
@@ -1,15 +1,19 @@
 using Finance.Application.UseCases.Bank.Commons;
 using Finance.Domain.Entities;
 using Finance.Domain.Repositories;
+using Finance.Domain.SeedWork;
 
 namespace Finance.Application.UseCases.Bank.CreateBank
 {
-    public class CreateBankHandler(IBankRepository bankRepository) : ICreateBankHandler
+    public class CreateBankHandler(
+        IBankRepository bankRepository,
+        IUnitOfWork unitOfWork) : ICreateBankHandler
     {
         public async Task<BankResponse> Handle(CreateBankRequest request, CancellationToken cancellationToken)
         {
             var bank = new BankEntity(code: request.Code, name: request.Name, color: request.Color, logo: request.Logo);
             await bankRepository.InsertAsync(bank, cancellationToken);
+            await unitOfWork.CommitAsync(cancellationToken);
 
             throw new NotImplementedException();
         }
