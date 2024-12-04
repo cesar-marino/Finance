43e3867ced9959ebc9ae0db3800c3c669680c798
César Marino <cesarmarino00@gmail.com>
2024-12-04 16:09:15 -0300

    add bank entity

--- src/Domain/Finance.Domain/Entities/BankEntity.cs
+++ src/Domain/Finance.Domain/Entities/BankEntity.cs
@@ -4,6 +4,36 @@ namespace Finance.Domain.Entities
 {
     public class BankEntity : AggregateRoot
     {
+        public bool Active { get; private set; }
+        public string? Code { get; private set; }
         public string Name { get; private set; }
+        public string? Color { get; private set; }
+        public string? Logo { get; private set; }
+
+        public BankEntity(string? code, string name, string? color, string? logo)
+        {
+            Active = true;
+            Code = code;
+            Name = name;
+            Color = color;
+            Logo = logo;
+        }
+
+        public BankEntity(
+            Guid bankId,
+            bool active,
+            string? code,
+            string name,
+            string? color,
+            string? logo,
+            DateTime createdAt,
+            DateTime updatedAt) : base(bankId, createdAt, updatedAt)
+        {
+            Active = active;
+            Code = code;
+            Name = name;
+            Color = color;
+            Logo = logo;
+        }
     }
 }
