ff4a519ad8fb41653be720862035eb3b16f4163b
César Marino <cesarmarino00@gmail.com>
2024-12-04 16:27:56 -0300

    ensure rethrow same exception that InsertAsync throws

--- src/Application/Finance.Application/UseCases/Bank/CreateBank/CreateBankHandler.cs
+++ src/Application/Finance.Application/UseCases/Bank/CreateBank/CreateBankHandler.cs
@@ -1,11 +1,16 @@
 using Finance.Application.UseCases.Bank.Commons;
+using Finance.Domain.Entities;
+using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Bank.CreateBank
 {
-    public class CreateBankHandler : ICreateBankHandler
+    public class CreateBankHandler(IBankRepository bankRepository) : ICreateBankHandler
     {
-        public Task<BankResponse> Handle(CreateBankRequest request, CancellationToken cancellationToken)
+        public async Task<BankResponse> Handle(CreateBankRequest request, CancellationToken cancellationToken)
         {
+            var bank = new BankEntity(code: request.Code, name: request.Name, color: request.Color, logo: request.Logo);
+            await bankRepository.InsertAsync(bank, cancellationToken);
+
             throw new NotImplementedException();
         }
     }
