a247ab8f0b52cb3ddfb7ccfb703eca8d81d7c1af
César Marino <cesarmarino00@gmail.com>
2024-12-14 16:39:58 -0300

    upload tests

--- src/Application/Finance.Application/UseCases/File/CreateFile/CreateFileRequest.cs
+++ src/Application/Finance.Application/UseCases/File/CreateFile/CreateFileRequest.cs
@@ -2,9 +2,9 @@ using MediatR;
 
 namespace Finance.Application.UseCases.File.CreateFile
 {
-    public class CreateFileRequest(string name, Stream Video) : IRequest<bool>
+    public class CreateFileRequest(string name, Stream? video) : IRequest<bool>
     {
         public string Name { get; } = name;
-        Stream Video { get; } = Video;
+        public Stream? Video { get; } = video;
     }
 }
--- src/Presentation/Finance.Presentation.API/Controllers/FileController.cs
+++ src/Presentation/Finance.Presentation.API/Controllers/FileController.cs
@@ -1,22 +1,29 @@
-using Finance.Application.UseCases.File.CreateFile;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Finance.Presentation.API.Controllers
 {
+
     [Route("[controller]")]
     [ApiController]
-    public class FileController(IMediator mediator) : Controller
+    public class FileController : Controller
     {
-        [HttpPost]
-        public async Task<IActionResult> Create(
-            IFormFile video,
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Create(
+            [FromRoute] string id,
+            [FromForm] IFormFile? file,
             CancellationToken cancellationToken = default)
         {
-            var stream = video.OpenReadStream();
-            var request = new CreateFileRequest("name", stream);
-            var response = await mediator.Send(request, cancellationToken);
-            return StatusCode(201, response);
+            if (file != null)
+                return Ok($"{id}: file is not null");
+
+            return Ok($"id: {id}");
         }
     }
 }
