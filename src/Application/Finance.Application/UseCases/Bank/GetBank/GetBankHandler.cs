1f44b30a7ad3e12613df2a12c26efe164faba566
César Marino <cesarmarino00@gmail.com>
2024-12-17 10:52:09 -0300

    ensure rethrow same exception that FindAsync throws

--- src/Application/Finance.Application/UseCases/Bank/GetBank/GetBankHandler.cs
+++ src/Application/Finance.Application/UseCases/Bank/GetBank/GetBankHandler.cs
@@ -1,11 +1,13 @@
 using Finance.Application.UseCases.Bank.Commons;
+using Finance.Domain.Repositories;
 
 namespace Finance.Application.UseCases.Bank.GetBank
 {
-    public class GetBankHandler : IGetBankHandler
+    public class GetBankHandler(IBankRepository bankRepository) : IGetBankHandler
     {
-        public Task<BankResponse> Handle(GetBankRequest request, CancellationToken cancellationToken)
+        public async Task<BankResponse> Handle(GetBankRequest request, CancellationToken cancellationToken)
         {
+            _ = await bankRepository.FindAsync(request.BankId, cancellationToken);
             throw new NotImplementedException();
         }
     }
