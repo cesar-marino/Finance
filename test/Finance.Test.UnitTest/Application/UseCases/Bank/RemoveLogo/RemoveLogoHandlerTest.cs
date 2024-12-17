cbc3494d37fdb47da5a16052d038e33e3a87cdca
César Marino <cesarmarino00@gmail.com>
2024-12-17 15:56:15 -0300

    add remove logo unit tests

---
+++ test/Finance.Test.UnitTest/Application/UseCases/Bank/RemoveLogo/RemoveLogoHandlerTest.cs
@@ -0,0 +1,7 @@
+namespace Finance.Test.UnitTest.Application.UseCases.Bank.RemoveLogo
+{
+    public class RemoveLogoHandlerTest : IClassFixture<RemoveLogoHandlerTestFixture>
+    {
+
+    }
+}
---
+++ test/Finance.Test.UnitTest/Application/UseCases/Bank/RemoveLogo/RemoveLogoHandlerTestFixture.cs
@@ -0,0 +1,9 @@
+using Finance.Test.UnitTest.Commons;
+
+namespace Finance.Test.UnitTest.Application.UseCases.Bank.RemoveLogo
+{
+    public class RemoveLogoHandlerTestFixture : FixtureBase
+    {
+
+    }
+}
